a027aadbda45e5e2fa4bb48aec5c9676502f8cf6
1134005157 <1134005157@qq.com>
2020-07-20 15:39:34 +0800

    springboot + vue

--- src/main/java/com/springboot/vue/controller/HelloController.java
+++ src/main/java/com/springboot/vue/controller/HelloController.java
@@ -1,10 +1,14 @@
 package com.springboot.vue.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "Hello SpringBoot-Vue";
+    }
 
 }
